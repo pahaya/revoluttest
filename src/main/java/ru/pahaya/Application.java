3b60595a91ea2be90823eaedbc9b75426053cffc
pahaya <pahaya@yandex.ru>
2019-11-03 22:59:41 +0300

    Jetty and servlet creation

--- src/main/java/ru/pahaya/Application.java
+++ src/main/java/ru/pahaya/Application.java
@@ -1,11 +1,46 @@
 package ru.pahaya;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import ru.pahaya.servlets.AccountServlet;
+import ru.pahaya.servlets.TransactionServlet;
 
-@SpringBootApplication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws Exception {
+//Create the server
+        int maxThreads = 100;
+        int minThreads = 10;
+        int idleTimeout = 120;
+
+        QueuedThreadPool threadPool = new QueuedThreadPool(maxThreads, minThreads, idleTimeout);
+
+        Server server = new Server(threadPool);
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8080);
+        server.setConnectors(new Connector[] {connector});
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+
+        context.addServlet(new ServletHolder(new TransactionServlet()), "/transaction/*");
+        context.addServlet(new ServletHolder(new AccountServlet()), "/account/*");
+
+
+        // Config and launch server
+        server.setHandler(context);
+        server.start();
+        server.dump(System.err);
+        server.join();
     }
 }
---
+++ src/main/java/ru/pahaya/servlets/TransactionServlet.java
@@ -0,0 +1,30 @@
+package ru.pahaya.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TransactionServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getOutputStream().println("Hi");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+}
